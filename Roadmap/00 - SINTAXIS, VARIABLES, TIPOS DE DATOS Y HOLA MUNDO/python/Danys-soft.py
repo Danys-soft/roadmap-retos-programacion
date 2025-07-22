32d8af6cb2cf54cd03fc5f5a696069b49a01d668
Danys-soft <montoya633z@gmail.com>
2025-07-21 22:49:09 -0500

    Create Danys-soft.py

---
+++ Roadmap/00 - SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO/python/Danys-soft.py
@@ -0,0 +1,37 @@
+# https://www.python.org/
+
+# Este es un comentario de una sola linea 
+"""
+Este es un comentario 
+de varias
+lineas
+"""
+'''
+Este tambien puede 
+ser otro
+de varias lineas
+'''
+
+# Constancia en Python no existe, puede ser que en otros programas de programacion sean distintas
+
+#segun en phython existe un constante FALSO, en la cual todo aquel que lo encuentre sabra que 
+#este no tendra que modificarse y es:
+
+MY_CONSTANT
+
+#en la cual para que una variable este en mayuscula es para que este no le cambie de valor
+
+#DATOS PRIMITIVOS
+my_int = 1 # numeros enteros
+my_float = 1.5 # numeros decimales
+my_bool = True
+# o
+my_bool = False 
+my_string = "cadena de texto"
+# o si se quiere referir a una distinta
+my_other_string = "otra cadena de texto"
+
+# el imprimir sirve para mostrar algo al iniciar el terminal
+
+print("¡Hola, Python!")
+
